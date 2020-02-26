f5247b1603b524a12fc4836b4feefc530cd3185e
zRagnnar <57605654+zRagnnar@users.noreply.github.com>
2020-02-26 18:01:37 -0300

    Update d86ouchebag.js

--- very/d86ouchebag.js
+++ very/d86ouchebag.js
@@ -1,29 +1,30 @@
-var NumberOfWords = 20
+var NumberOfWords = 21
 var words = new BuildArray(NumberOfWords)
 
 // Use the following variables to 
 // define your random words:
 
 words[1] = "https://throwbin.io/7kDTKtx"
-words[2] = "https://throwbin.io/lmzXshz"
-words[3] = "https://throwbin.io/C0LjapH"
-words[4] = "https://throwbin.io/PJkaXMD"
-words[5] = "https://throwbin.io/tVywqU4"
-words[6] = "ttps://throwbin.io/6gKI226"
-words[7] = "https://throwbin.io/1aIuIud"
-words[8] = "https://throwbin.io/AMXfVZS"
-words[9] = "https://throwbin.io/NFguiC2"
-words[10] = "https://throwbin.io/hOjUiDU"
-words[11] = "https://throwbin.io/I4QyHJG"
-words[12] = "https://throwbin.io/64Gwtg6"
-words[13] = "ttps://throwbin.io/RW8uHhH"
-words[14] = "https://throwbin.io/HVL3b21"
-words[15] = "https://throwbin.io/0D92pur"
-words[16] = "https://throwbin.io/9gpzzxx"
-words[17] = "https://throwbin.io/VWIDDyj"
-words[18] = "https://throwbin.io/XZPEBeg"
-words[19] = "https://throwbin.io/w2qo9eY"
-words[20] = "https://throwbin.io/SPaCecJ"
+words[2] = "https://enrt.eu/1L1qJmw"
+words[3] = "https://enrt.eu/9PaK"
+words[4] = "https://enrt.eu/0TY35"
+words[5] = "https://enrt.eu/rGky"
+words[6] = "https://enrt.eu/XAyl"
+words[7] = "https://enrt.eu/ZOLlC"
+words[8] = "https://enrt.eu/5gmnM"
+words[9] = "https://enrt.eu/NUcz"
+words[10] = "https://enrt.eu/sNQ8u5"
+words[11] = "https://enrt.eu/uJHNtBYT"
+words[12] = "https://enrt.eu/ruxbs"
+words[13] = "https://enrt.eu/n8ldytXZ"
+words[14] = "https://enrt.eu/6BbSou"
+words[15] = "https://enrt.eu/G1RqI3"
+words[16] = "https://enrt.eu/tGaO"
+words[17] = "https://enrt.eu/lkop3b"
+words[18] = "https://enrt.eu/ILvBN"
+words[19] = "https://enrt.eu/1CQA"
+words[20] = "https://enrt.eu/GM6fiB"
+words[21] = "https://enrt.eu/jqZYd3RY"
 
 function BuildArray(size){
 this.length = size
