d8a0c203be6840f30a805bb1171f86bfba06228e
zRagnnar <57605654+zRagnnar@users.noreply.github.com>
2020-02-28 22:42:58 -0300

    Update d86ouchebag.js

--- very/d86ouchebag.js
+++ very/d86ouchebag.js
@@ -1,29 +1,13 @@
-var NumberOfWords = 20
+var NumberOfWords = 4
 var words = new BuildArray(NumberOfWords)
 
 // Use the following variables to 
 // define your random words:
 
-words[1] = "https://enrt.eu/1L1qJmw"
-words[2] = "https://enrt.eu/9PaK"
-words[3] = "https://enrt.eu/0TY35"
-words[4] = "https://enrt.eu/rGky"
-words[5] = "https://enrt.eu/XAyl"
-words[6] = "https://enrt.eu/ZOLlC"
-words[7] = "https://enrt.eu/5gmnM"
-words[8] = "https://enrt.eu/NUcz"
-words[9] = "https://enrt.eu/sNQ8u5"
-words[10] = "https://enrt.eu/uJHNtBYT"
-words[11] = "https://enrt.eu/ruxbs"
-words[12] = "https://enrt.eu/n8ldytXZ"
-words[13] = "https://enrt.eu/6BbSou"
-words[14] = "https://enrt.eu/G1RqI3"
-words[15] = "https://enrt.eu/tGaO"
-words[16] = "https://enrt.eu/lkop3b"
-words[17] = "https://enrt.eu/ILvBN"
-words[18] = "https://enrt.eu/1CQA"
-words[19] = "https://enrt.eu/GM6fiB"
-words[20] = "https://enrt.eu/jqZYd3RY"
+words[1] = "https://shrinkurl.org/8rA0H8r"
+words[2] = "https://shrinkurl.org/0jae"
+words[3] = "https://shrinkurl.org/rv3ZAZR4"
+words[4] = "https://shrinkurl.org/Ja9Y1Jdo"
 
 function BuildArray(size){
 this.length = size
